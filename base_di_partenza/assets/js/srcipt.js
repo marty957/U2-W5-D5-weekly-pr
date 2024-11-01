10d53f4e6b7f3d86a31a17e1c555bff872052c0e
marty957 <fantuzzimartina@yahoo.it>
2024-11-01 14:23:32 +0100

    navBar animation done

--- base_di_partenza/assets/js/srcipt.js
+++ base_di_partenza/assets/js/srcipt.js
@@ -1,11 +1,18 @@
 const navBar = document.querySelector(".animated_nav");
-const header = document.querySelector("header");
+const header = document.getElementById("header");
 const buttonNav = document.querySelector("nav button");
 console.log(navBar);
-console.log(navBar.getBoundingClientRect().height);
-console.log(header.getBoundingClientRect().height);
+console.log(header.offsetHeight);
+const height2 = header.offsetHeight;
+console.log(height2);
 
 window.onscroll = function () {
-  navBar.classList.add("white");
-  buttonNav.classList.add("green");
+  let height1 = window.scrollY;
+  if (height1 >= height2) {
+    navBar.classList.add("white");
+    buttonNav.classList.add("green");
+  } else {
+    navBar.classList.remove("white");
+    buttonNav.classList.remove("green");
+  }
 };
